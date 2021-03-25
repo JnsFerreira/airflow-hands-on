ad25dc4fa8bcd1f85564bfbe40743fcda18b6b18
Jonas Ferreira <jonas.abreuferreira@gmail.com>
2021-03-24 22:10:12 -0300

    Creating custom macros

--- dags/testing_variables.py
+++ dags/testing_variables.py
@@ -2,30 +2,33 @@ from airflow import DAG
 from datetime import datetime, timedelta
 from airflow.operators.bash_operator import BashOperator
 
-def get_last_dag_run():
-    return "some_value"
+def get_start_date(base_date):
+    return 'start_date'
+
+def get_end_date(base_date):
+    return 'end_date'
 
 default_args = {
     'owner': 'Airflow',
-    'start_date': datetime(2021, 3, 21),
+    'start_date': datetime(2021, 3, 22),
     'retries': 1,
     'retry_delay': timedelta(seconds=30)
 }
 
-
 with DAG(
-    dag_id='testing_variables', 
+    dag_id='testing_variabl5', 
     default_args=default_args, 
     schedule_interval='@daily', 
     catchup=True, 
     user_defined_macros={
-          'last_dag_run_execution_date': get_last_dag_run
+          'start_date' : get_start_date,
+          'retireve_end_date' : get_end_date 
       }
 ) as dag:
 
     my_bash = BashOperator(
-        task_id="date",
-        bash_command="echo SELECT * FROM  my_table WHERE date >= {{ last_dag_run_execution_date() }}"
+        task_id="GET",
+        bash_command="echo {{ retireve_end_date( prev_execution_date_success )}} maior que {{ start_date( ds ) }}"
     )
 
     my_bash
