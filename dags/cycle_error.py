f063983d2e9d7f3879cf16102361b7ae9ddfcc98
Jonas Ferreira <jonas.abreuferreira@gmail.com>
2021-05-16 18:20:24 -0300

    Improving tests

--- dags/cycle_error.py
+++ dags/cycle_error.py
@@ -1,7 +1,20 @@
-from airflow import DAG
-from airflow.operators.dummy_operator import DummyOperator
+from datetime import datetime, timedelta
 
-with DAG() as dag:
+from airflow import DAG
+from airflow.operators.dummy import DummyOperator
+
+default_args = {
+    'owner': 'Airflow',
+    'start_date': datetime(2021, 3, 22),
+    'retries': 1,
+    'retry_delay': timedelta(seconds=30)
+}
+
+with DAG(
+    dag_id='cycle_error',
+    default_args=default_args,
+    schedule_interval='None'
+) as dag:
     t1 = DummyOperator(task_id='t1')
     t2 = DummyOperator(task_id='t2')
     t3 = DummyOperator(task_id='t3')
